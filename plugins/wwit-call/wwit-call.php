74b803f784c1032c3d92a544aa93e2e1d1d62ea2
Judy Beegle <jbeegle@us.ibm.com>
2015-11-13 16:31:25 -0600

    Add test plugins for implementing wwit app functions.

---
+++ plugins/wwit-call/wwit-call.php
@@ -0,0 +1,90 @@
+<?php
+/*
+Plugin Name: WWIT Call
+Plugin URI: http://example.com
+Description: Simple http call
+Version: 1.0
+Author: ITSM WW IT
+Author URI: http://w3guy.com
+*/
+
+function call_form() {
+  echo '<form action="' . esc_url($_SERVER['REQUEST_URI']) . '" method="post">';
+  echo '<p>';
+  echo 'Your Name (required) <br/>';
+  echo '<input type="text" name="call-name" pattern="[a-zA-Z0-9@. ]+" value="' . (isset($_POST["call-name"]) ? esc_attr($_POST["call-name"]) : '') . '" size="40" />';
+  echo '</p>';
+  echo '<p>';
+  echo 'Your Password (required) <br/>';
+  echo '<input type="password" name="http-password" value="' . (isset($_POST["http-password"]) ? esc_attr($_POST["http-password"]) : '') . '" size="40" />';
+  echo '</p>';
+  echo '<p>';
+  echo 'Your Email (required) <br/>';
+  echo '<input type="email" name="call-email" value="' . (isset($_POST["call-email"]) ? esc_attr($_POST["call-email"]) : '') . '" size="40" />';
+  echo '</p>';
+  echo '<p>';
+  echo 'Application (required)<br/>';
+  echo '<input type="text" name="call-application" pattern="[a-zA-Z ]+" value="' . (isset($_POST["call-application"]) ? esc_attr($_POST["call-application"]) : '') . '" size="40" />';
+  echo '</p>';
+  echo '<p><input type="submit" name="call-submitted" value="Send"></p>';
+  echo '</form>';
+}
+ 
+function call_http(){
+  // if the submit button is clicked, make the call
+  if (isset($_POST['call-submitted'])){
+    // sanitize form values
+    $name = sanitize_text_field($_POST["call-name"]);
+    $email = sanitize_email($_POST["call-email"]);
+    $application = sanitize_text_field($_POST["call-application"]);
+    $password = sanitize_text_field($_POST["http-password"]);
+
+    // vLaunch test site
+    $url = 'https://vlaunch.rtp.raleigh.ibm.com/groups';
+
+    // UCD test site
+    // $url = 'https://rtpucd01-srv.tivlab.raleigh.ibm.com:8443/cli/application/';
+
+    // vLaunch create VM
+    // $url = 'https://vlaunch.rtp.raleigh.ibm.com/newrequests/RTP/1/createVM';
+
+    $args = array(
+      'headers' => array(
+        'Authorization' => 'Basic ' . base64_encode($name . ':' . $password)
+      ), 'sslverify' => false
+    );
+    $response = wp_remote_get($url, $args);
+    $msg = wp_remote_retrieve_response_message ($response);
+    // If not empty, display message
+    if (! empty($msg)){
+      echo 'Message: <pre>';
+      print_r ($msg);
+    echo '</pre>';
+    }
+    $http_code = wp_remote_retrieve_response_code ($response);
+    echo 'Code: <pre>';
+    print_r ($http_code);
+    echo '</pre>';
+    // If call processed display success
+    if ( $response_code == '200' ) {
+      echo '<p>Success!</p>';
+      }
+
+    echo 'Response: <pre>';
+    print_r ($response);
+    // var_dump ($response);
+    echo '</pre>';
+
+    $body = wp_remote_retrieve_body($response);
+    echo '<p>Body:' . $body . '</p>';
+  } //end if ( isset( $_POST['call-submitted'] ) )
+} // end function call_mail
+
+function call_shortcode() {
+  ob_start();
+  call_http();
+  call_form();
+  return ob_get_clean();
+}
+add_shortcode( 'wwit-call', 'call_shortcode' );
+?>
---
+++ plugins/wwit-http-call/wwit-vm-call.php
@@ -0,0 +1,95 @@
+<?php
+/*
+Plugin Name: WWIT VM HTTP Call Plugin
+Plugin URI: http://example.com
+Description: Simple HTTP Call for WW IT Resources and Services
+Version: 2.0
+Author: ITSM WW IT
+Author URI: http://w3guy.com
+// */
+function html_http_code() {
+if ( !isset( $_POST['http-submitted'] ) ){
+    echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
+    echo '<p>';
+    echo 'Your Name (required) <br/>';
+    echo '<input type="text" name="http-name" pattern="[a-zA-Z0-9@. ]+" value="' . ( isset( $_POST["http-name"] ) ? esc_attr( $_POST["http-name"] ) : '' ) . '" size="40" />';
+    echo '</p>';
+    echo '<p>';
+    echo 'Your Password (required) <br/>';
+    echo '<input type="password" name="http-password" value="' . ( isset( $_POST["http-password"] ) ? esc_attr( $_POST["http-password"] ) : '' ) . '" size="40" />';
+    echo '</p>';
+    echo '<p>';
+    echo 'Your Email (required) <br/>';
+    echo '<input type="email" name="http-email" value="' . ( isset( $_POST["http-email"] )? esc_attr( $_POST["http-email"] ) : '' ) . '" size="40" />';
+    echo '</p>';
+    echo '<p>';
+    echo 'Application (required) <br/>';
+    echo '<input type="text" name="http-application" pattern="[a-zA-Z ]+" value="' . ( isset( $_POST["http-application"] ) ? esc_attr( $_POST["http-aplication"] ) : '' ) . '" size="40" />';
+    echo '</p>';
+    echo '<p><input type="submit" name="http-submitted" value="Send"></p>';
+    echo '</form>';
+   }
+}
+ 
+function http_call() {
+ 
+    // if the submit button is clicked, send the email
+    if ( isset( $_POST['http-submitted'] ) ) {
+ 
+        // sanitize form values
+        $name    = sanitize_text_field( $_POST["http-name"] );
+        $email   = sanitize_email( $_POST["http-email"] );
+        $application = sanitize_text_field( $_POST["http-application"] );
+        $password = $_POST["http-password"];
+
+//        $DispForm = False;
+
+        // setup the http call
+
+/* UCD test site
+$url = 'https://rtpucd01-srv.tivlab.raleigh.ibm.com:8443/cli/application/';
+*/
+
+/* vLaunch test site
+$url = 'https://vlaunch.rtp.raleigh.ibm.com/groups';
+*/
+
+$url = 'https://rtpucd01-srv.tivlab.raleigh.ibm.com:8443/cli/application/';
+$args = array(
+    'headers' => array(
+        'Authorization' => 'Basic ' . base64_encode( $name . ':' . $password)
+    ), 'sslverify' => false
+);
+  $response = wp_remote_get( $url, $args );
+  print_r( $response  );
+
+  $response_code =  wp_remote_retrieve_response_code( $response );
+ 
+  print_r( $response_code  );
+        
+        // If http has been processed, display a success message
+        if ( $response_code == '200' ) {
+            echo '<div>';
+            echo '<p>http call successful, result is:</p>';
+            echo '</div>';
+ 
+ $body = wp_remote_retrieve_body( $response );
+      	 print_r( $response_code  );
+ 
+        } else {
+            echo 'An unexpected error occurred';
+        }
+    }
+}
+ 
+function http_shortcode() {
+    ob_start();
+    http_call();
+    html_http_code();
+    
+    return ob_get_clean();
+}
+ 
+add_shortcode( 'wwit_http_call', 'http_shortcode' );
+ 
+?>
