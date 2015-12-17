ce4f812ccdb49a0b99fad0a23f9ce7f12207595f
Judy Beegle <jbeegle@us.ibm.com>
2015-12-17 16:40:49 -0600

    Complete deployment of Ubuntu 14.04 VM

--- plugins/wwit-vm-deploy/wwit-vm-deploy.php
+++ plugins/wwit-vm-deploy/wwit-vm-deploy.php
@@ -25,19 +25,24 @@ function deploy_vm_form() {
 function check_error($response){
   $msg = wp_remote_retrieve_response_message ($response);
   $code = wp_remote_retrieve_response_code ($response);
-  echo '<br>Code = ';
-  print_r ($code);
-  echo ', ';
-  print_r ($msg);
+  echo '<br>Code = '.$code.', '.$msg;
   switch ($code) {
     case '200':
       echo ' - Success!';
+      break;
+    case '201':
+      $body = wp_remote_retrieve_body($response);
+      echo '<br>';
+      print_r ($body);
       break;
     case '401':
       echo ' - Insufficient privileges, bad credentials, or malformed request.';
       break;
     case '404':
       echo ' - Endpoint does not exist.';
+      break;
+    case '413':
+//      echo ' - Request Entity too Large.';
       break;
     case '422':
       echo ' - No valid search predicate.';
@@ -48,10 +53,11 @@ function check_error($response){
       $body = wp_remote_retrieve_body($response);
       echo '<br>';
       print_r ($body);
-  }
+  } // end $code !== 200
   echo '<br>';
   return $code;
-}
+} // end function check_error
+
 function deploy_vm(){
   $host = 'https://vlaunch.rtp.raleigh.ibm.com/';
 
@@ -65,21 +71,22 @@ function deploy_vm(){
       'sslverify' => false
       ); // end arg definition
 
+    echo '<b>Get token</b> ';
     // vLaunch authentication - get authorization
     $url = $host . 'api/token';
     $response = wp_remote_get($url, $args);
-    echo '<b>Get token</b> ';
     $http_code = check_error ($response);
     if ($http_code == '200'){
     //  Parse API request authorization token from response body
         $body = wp_remote_retrieve_body($response);
         $auth = (explode('"', $body));
         $token = $auth[7];
-    }
+        echo 'Token: '.$token.'<br>';
+    } // end $http_code == '200'
 
     // vLaunch - create VM
 
-    $args[headers] [Authorization] = 'Token token=' . $token;
+    // $args[headers] [Authorization] = 'Token token=' . $token;
 
     // This url gets a list of the mgmt servers to which I have access
     // I will use ID=136 (visvc6A) 
@@ -91,19 +98,46 @@ function deploy_vm(){
     // $url = $host . 'api/v1/workflows';
     // echo '<b>Get workflows</b> ';
 
+    // Show request details; e.g., errors
+    // $url = $host . 'api/v1/requests/178514';
+    // echo '<b>Show Request</b> ';
+
     // This url gets a list of all workflow templates on the servers I can access
     // Using ID 151 (UB14-4-64SVR) for Ubuntu 14.04 Server 64-bit on visvc6a
     // $url = $host . 'api/v1/templates';
     // echo '<b>Get templates</b> ';
 
+    // Get list of VM owners, filtered my email to determine my owner ID
+    // Using ID 27274
+    // $url = $host . 'api/v1/owners?email';
+    // echo '<b>Get owners</b> ';
+    // $args[body] = 'q[email_cont]=jbeegle';
+
     // $response = wp_remote_get($url, $args);
 
-    $url = $host . 'api/v1/actions/deployvm/136'; //Deploy a New VM
     // $url = $host . 'api/v1/requests'; // Create Request
-    // $url = $host . 'newrequests/RTP/1/createVM';
+
+    $url = $host . 'api/v1/actions/deployvm/136'; //Deploy a New VM
 
     echo '<b>Deploy VM</b> ';
-    $response = wp_remote_post($url, $args);
+                                    $t=time();
+    $deploy_args = array(
+      'headers' => array('Authorization' => 'Token token=' . $token),
+      'body' => array(
+        'server_id' => '136', 
+        'hostname' => 'judy-test-' . time(), // make hostname unique; multiple deployments with the same name fail
+        'domain' => 'rtp.raleigh.ibm.com', 
+        'template' => 'UB14-4-64SVR',
+        'owner_id' => '27274', 
+        'cpu' => '2', 'ram' => '1', 
+        'itsas-auto-patch' => 'Weekends NO reboot'),
+      'sslverify' => false
+      ); // end arg definition
+
+    echo '<br>DeployArgs: ';
+    var_dump ($deploy_args);
+    echo '<br>';
+    $response = wp_remote_post($url, $deploy_args);
     check_error ($response);
     echo 'Response: <pre>';
     print_r ($response);
